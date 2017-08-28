d80863404039a56c4a672b1d4708b838cfdf85a6
PanidaKhup <bell849948@gmail.com>
2017-08-28 10:58:05 +0700

    close class runnablemanual

--- BookSeat/src/main/ThreadMain.java
+++ BookSeat/src/main/ThreadMain.java
@@ -93,37 +93,37 @@ class RunnableAuto implements Runnable {
 		   
 }
 
-class RunnableManual implements Runnable {
-	   private Thread t;
-	   private String threadName;
+// class RunnableManual implements Runnable {
+// 	   private Thread t;
+// 	   private String threadName;
 	   
-	   RunnableManual(String name) {
-	      threadName = name;
-	      System.out.println("Creating " +  threadName );
-	   }
+// 	   RunnableManual(String name) {
+// 	      threadName = name;
+// 	      System.out.println("Creating " +  threadName );
+// 	   }
 	   
-	   public void run() {
-	      System.out.println("Running " +  threadName );
-	      try {
-	         for(int i = 4; i > 0; i--) {
-	            System.out.println("Thread: " + threadName + ", " + i);
-	            // Let the thread sleep for a while.
-	            Thread.sleep(50);
-	         }
-	      }catch (InterruptedException e) {
-	         System.out.println("Thread " +  threadName + " interrupted.");
-	      }
-	      System.out.println("Thread " +  threadName + " exiting.");
-	   }
+// 	   public void run() {
+// 	      System.out.println("Running " +  threadName );
+// 	      try {
+// 	         for(int i = 4; i > 0; i--) {
+// 	            System.out.println("Thread: " + threadName + ", " + i);
+// 	            // Let the thread sleep for a while.
+// 	            Thread.sleep(50);
+// 	         }
+// 	      }catch (InterruptedException e) {
+// 	         System.out.println("Thread " +  threadName + " interrupted.");
+// 	      }
+// 	      System.out.println("Thread " +  threadName + " exiting.");
+// 	   }
 	   
-	   public void start () {
-	      System.out.println("Starting " +  threadName );
-	      if (t == null) {
-	         t = new Thread (this, threadName);
-	         t.start ();
-	      }
-	   }
-}
+// 	   public void start () {
+// 	      System.out.println("Starting " +  threadName );
+// 	      if (t == null) {
+// 	         t = new Thread (this, threadName);
+// 	         t.start ();
+// 	      }
+// 	   }
+// }
 
 public class ThreadMain {
 
